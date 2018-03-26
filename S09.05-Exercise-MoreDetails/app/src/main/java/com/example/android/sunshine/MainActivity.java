d57742d7a5eb50267f48a8ad9e0c7896bccaa13e
DESKTOP-K7OPVS1\katie <katie.sang.park@gmail.com>
2018-03-25 20:27:18 -0700

    COMPLETED todos S09.05 More Details

--- S09.05-Exercise-MoreDetails/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S09.05-Exercise-MoreDetails/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -262,16 +262,15 @@ public class MainActivity extends AppCompatActivity implements
     /**
      * This method is for responding to clicks from our list.
      *
-     * @param weatherForDay String describing weather details for a particular day
+     * @param date  String describing weather details for a particular day
      */
     @Override
-    public void onClick(String weatherForDay) {
+    public void onClick(long date) {
+        Intent weatherDetailIntent = new Intent(MainActivity.this, DetailActivity.class);
 //      TODO (39) Refactor onClick to build a URI for the clicked date and and pass it with the Intent using setData
-        Context context = this;
-        Class destinationClass = DetailActivity.class;
-        Intent intentToStartDetailActivity = new Intent(context, destinationClass);
-        intentToStartDetailActivity.putExtra(Intent.EXTRA_TEXT, weatherForDay);
-        startActivity(intentToStartDetailActivity);
+        Uri uriForDateClicked = WeatherContract.WeatherEntry.buildWeatherUriWithDate(date);
+        weatherDetailIntent.setData(uriForDateClicked);
+        startActivity(weatherDetailIntent);
     }
 
     /**
